2cad8c517ee8e7c4e2383ae9acbffce011cf9891
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2017-12-28 03:20:37 +0600

    TableSeparator added on service list

--- src/Commands/ServiceListCommand.php
+++ src/Commands/ServiceListCommand.php
@@ -3,6 +3,7 @@
 use Portner\Service\Storage;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -28,16 +29,21 @@ class ServiceListCommand extends Command
 			return;
 		}
 
-		$table = new Table($output);
-		$table->setHeaders([ 'Service Name', 'Actual Port', 'Host port expose at', 'Last used port' ]);
-		$table->setRows(array_map(function ($row) {
-			return [
-				'name'           => $row['name'],
-				'port'           => $row['port'],
-				'expose_at'      => $row['expose_at'],
-				'last_used_port' => array_key_exists('last_used_port', $row) ? $row['last_used_port'] : '',
+		$rows = [];
+		foreach ($services as $service) {
+			$rows[] = [
+				'name'           => $service['name'],
+				'port'           => $service['port'],
+				'expose_at'      => $service['expose_at'],
+				'last_used_port' => array_key_exists('last_used_port', $service) ? $service['last_used_port'] : '',
 			];
-		}, $services));
-		$table->render();
+			if (next($services)) {
+				$rows[] = new TableSeparator();
+			}
+		}
+		
+		(new Table($output))->setHeaders([ 'Service Name', 'Actual Port', 'Host port expose at', 'Last used port' ])
+							->setRows($rows)
+							->render();
 	}
 }
